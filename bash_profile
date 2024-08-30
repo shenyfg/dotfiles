95c4a8e76a454854553b74ce75a06e0ab6897995
shenyfg <shenyifeng2333@gmail.com>
2024-08-30 11:06:45 +0800

    Update bash-completion, tmux and gitignore

--- bash_profile
+++ bash_profile
@@ -1,55 +1,44 @@
 # Load the shell dotfiles, and then some:
 # * ~/.path can be used to extend `$PATH`.
 # * ~/.extra can be used for other settings you don’t want to commit.
+
+# Load bash_completion in homebrew dir
+[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+
+# Source files below if exist
 for file in ~/.{path,bash_prompt,exports,aliases,functions,extra,proxy}; do
-	[ -r "$file" ] && source "$file";
-done;
-unset file;
+  [ -r "$file" ] && source "$file"
+done
+unset file
 
 # use vim as default
 set -o vi
 
 # Disable ctrl s to lock screen
 stty stop ""
-# Disable ctrl d to exit bash
-# set -o ignoreeof
 
 # Activate inputrc
 bind -f ~/.inputrc
 
 # Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob;
+shopt -s nocaseglob
 
 # Append to the Bash history file, rather than overwriting it
-shopt -s histappend;
+shopt -s histappend
 
 # Autocorrect typos in path names when using `cd`
-shopt -s cdspell;
+shopt -s cdspell
 
 # Enable some Bash 4 features when possible:
 # * `autocd`, e.g. `**/qux` will enter `./foo/bar/baz/qux`
 # * Recursive globbing, e.g. `echo **/*.txt`
 for option in autocd globstar; do
-	shopt -s "$option" 2> /dev/null;
-done;
-
-# Add tab completion for many Bash commands
-if which brew &> /dev/null && [ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]; then
-	# Ensure existing Homebrew v1 completions continue to work
-	export BASH_COMPLETION_COMPAT_DIR="$(brew --prefix)/etc/bash_completion.d";
-	source "$(brew --prefix)/etc/profile.d/bash_completion.sh";
-elif [ -f /etc/bash_completion ]; then
-	source /etc/bash_completion;
-fi;
-
-# Enable tab completion for `g` by marking it as an alias for `git`
-if type _git &> /dev/null; then
-	complete -o default -o nospace -F _git g;
-fi;
+  shopt -s "$option" 2>/dev/null
+done
 
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
-[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh
 
 # Add tab completion for `defaults read|write NSGlobalDomain`
 # You could just use `-g` instead, but I like being explicit
-complete -W "NSGlobalDomain" defaults;
+complete -W "NSGlobalDomain" defaults
--- run.sh
+++ run.sh
@@ -1,48 +1,48 @@
-CONFIG_FILES=("clang-format" "tmux.conf" "aliases" "bash_profile" "bash_prompt" "bashrc" "bash_completion" "exports" "functions" "inputrc" "gitconfig" "gitignore" "gitmsg" "proxy" )
+CONFIG_FILES=("clang-format" "tmux.conf" "aliases" "bash_profile" "bash_prompt" "bashrc" "exports" "functions" "inputrc" "gitconfig" "gitignore" "gitmsg" "proxy")
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 function create_soft_link() {
-    destfile=$1
-    dotfile=$2
+  destfile=$1
+  dotfile=$2
 
-    # if symbolic file exists, delete it first
-    if [ -L "$dotfile" ]; then
-        rm -f "$dotfile"
-    fi
+  # if symbolic file exists, delete it first
+  if [ -L "$dotfile" ]; then
+    rm -f "$dotfile"
+  fi
 
-    # if real file exists, backup it
-    if [ -f "$dotfile" ]; then
-        mv "$dotfile" "$dotfile"_bak
-    fi
+  # if a real file exists, backup it first
+  if [ -f "$dotfile" ]; then
+    mv "$dotfile" "$dotfile"_bak
+  fi
 
-    ln -s "${destfile}" "${dotfile}"
+  ln -s "${destfile}" "${dotfile}"
 }
 
 function main() {
-    for f in ${CONFIG_FILES[*]}; do
-        dst_file="${SCRIPT_DIR}/${f}"
-        dot_file="${HOME}/.${f}"
-        create_soft_link "${dst_file}" "${dot_file}"
-    done
+  for f in ${CONFIG_FILES[*]}; do
+    dst_file="${SCRIPT_DIR}/${f}"
+    dot_file="${HOME}/.${f}"
+    create_soft_link "${dst_file}" "${dot_file}"
+  done
 
-    if [[ $(uname -a) == *"microsoft"* ]]; then
-        username=$(cmd.exe /c echo %username% | sed -e "s/\r//g")
-        echo "WSL environment, user is ${username}"
+  if [[ $(uname -a) == *"microsoft"* ]]; then
+    username=$(cmd.exe /c echo %username% | sed -e "s/\r//g")
+    echo "WSL environment, user is ${username}"
 
-        user_home="/mnt/c/Users/${username}"
-        terminal_path="${user_home}/AppData/Roaming/alacritty/"
-        ssh_path="${user_home}/.ssh"
+    user_home="/mnt/c/Users/${username}"
+    terminal_path="${user_home}/AppData/Roaming/alacritty/"
+    ssh_path="${user_home}/.ssh"
 
-        if [ ! -d "${ssh_path}" ]; then
-            echo "Start to copy ssh configs to ${ssh_path}"
-            cp -fr "${HOME}/.ssh" "${ssh_path}"
-        fi
-
-        echo "Start to copy terminal configs to ${terminal_path}"
-        cp -f alacritty.toml "${terminal_path}"
+    if [ ! -d "${ssh_path}" ]; then
+      echo "Start to copy ssh configs to ${ssh_path}"
+      cp -fr "${HOME}/.ssh" "${ssh_path}"
     fi
 
-    source ~/.bash_profile
+    echo "Start to copy terminal configs to ${terminal_path}"
+    cp -f alacritty.toml "${terminal_path}"
+  fi
+
+  source ~/.bash_profile
 }
 
 main
